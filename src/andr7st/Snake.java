321212a31049f30acbb42e1ce8c993fc0866b02c
Andres Segura <segura1st@outlook.es>
2021-01-05 13:16:48 -0500

    Finalizado: java-t

--- src/andr7st/Snake.java
+++ src/andr7st/Snake.java
@@ -4,22 +4,27 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
 
-public class Snake extends JFrame{
-
+public class Snake extends JFrame{ private static final long serialVersionUID = 1L;
     int widht_ = 640;
     int height_ = 480;
     Point snake;
     Point comida;
     ImagenSnake imagenSnake;
+    ArrayList<Point> lista;// = new ArrayList<Point>();
+
+    boolean gameOver = false;
+
     int widthPoint  = 10; 
     int heightPoint = 10;
 
-    long frecuencia = 20; // milisegundos
+    long frecuencia = 25; // milisegundos
 
     int direccion = KeyEvent.VK_LEFT;
 
@@ -54,23 +59,80 @@ public class Snake extends JFrame{
         comida = new Point(200,200);
         snake = new Point(widht_/2, height_/2);
 
+        lista = new ArrayList<Point>();
+        //lista.add(snake);
+
+        crearComida();
+    }
+    public void crearComida() {
+        Random rdm = new Random();
+
+        comida.x = rdm.nextInt(widht_);
+        comida.y = rdm.nextInt(height_);
+        
+        ////// x
+        if((comida.x % 5) > 0 ) {
+            comida.x = comida.x - (comida.x % 5);
+        }
+        if (comida.x < 5) {
+            comida.x = comida.x + 10;
+        }
+
+        ////// y
+        if((comida.y % 5) > 0 ) {
+            comida.y = comida.y - (comida.y % 5);
+        }
+        if (comida.y < 5) {
+            comida.y = comida.y + 10;
+        }
+
     }
     public void actualizar() {
         imagenSnake.repaint();
+
+        lista.add(0, new Point(snake.x,snake.y));
+        lista.remove((lista.size()-1));
+
+        for(int i=1; i<lista.size(); i++) {
+            Point punto = lista.get(i);
+
+            if(snake.x == punto.x && snake.y == punto.y) {
+                gameOver = true;
+            }
+        }
+
+        if((snake.x > (comida.x - 10)) && (snake.x < (comida.x + 10))  && (snake.y > (comida.y - 10)) && (snake.y < (comida.y + 10)) ){
+            /// Si se cumple todo esto es porque está dentro de la zona.
+            /// cada vez que la serpiente come un punto, se creará otro punto de comida.
+
+            lista.add(0, new Point(snake.x,snake.x));
+            crearComida();
+        }
     }
 
 
-    public class ImagenSnake extends JPanel {
-        
+    public class ImagenSnake extends JPanel {  private static final long serialVersionUID = 1L;
+
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
 
             g.setColor(new Color(0,0,255));
+
             g.fillRect(snake.x, snake.y, widthPoint, heightPoint);
-            
+
+            for (int i=0; i<lista.size(); i++) {
+                
+                Point point = (Point)lista.get(i);
+
+                g.fillRect(point.x, point.y, widthPoint, heightPoint);
+            }
             
             g.setColor(new Color(255,0,0));
             g.fillRect(comida.x, comida.y, widthPoint, heightPoint);
+
+            if(gameOver) {
+                g.drawString("GAME OVER", 200, 320);
+            }
         }
     }
 
@@ -108,14 +170,12 @@ public class Snake extends JFrame{
 		
 		private long last = 0;
 		
-		public Momento() {
-			
-		}
-
+        public Momento() {}
+        
 		public void run() {
 			while(true) {
 				if((java.lang.System.currentTimeMillis() - last) > frecuencia) {
-					//if(!gameOver) {
+					if(!gameOver) {
 
                         if(direccion == KeyEvent.VK_RIGHT) {
                             snake.x = snake.x + widthPoint;
@@ -138,7 +198,7 @@ public class Snake extends JFrame{
                                 snake.y = 0;
                             }                        
                         }
-                   // }
+                    }
                     actualizar();
 					
 					last = java.lang.System.currentTimeMillis();
